1086713825ed33312deb6abe1f68d83d7566d0a1
AriPunavuori <38551509+AriPunavuori@users.noreply.github.com>
2022-11-05 18:12:00 +0200

    2D Character controller

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+using UnityEngine; 
 public class Character2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController controller;
+    
+    private const float playerSpeed = 2.0f;
+    
+    private void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
         
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
     }
 }
