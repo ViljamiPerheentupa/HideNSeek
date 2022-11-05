283b2d470cccaeb72f725558ab19ec15ffee992f
itsaca <jenna.aaltonen@laajasalonopisto.fi>
2022-11-05 18:22:46 +0200

    Basic player movement with controller

--- Assets/Scripts/playermanager.cs
+++ Assets/Scripts/playermanager.cs
@@ -2,23 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playermanager : MonoBehaviour {
+public class PlayerManager : MonoBehaviour {
     private CharacterController cc;
     public float speed = 2.0f;
     Vector2 rotation = Vector2.zero;
     public float rotationSpeed = 3;
+    public Transform head;
+    public float headRotationSpeed = 3;
+    Vector2 headRotation = Vector2.zero;
+    public float headAngle = 0;
 
     void Start() {
         cc = GetComponent<CharacterController>();
     }
 
     void Update() {
-        Vector3 move = new Vector3(Input.GetAxis("VerticalJoystick"), 0, Input.GetAxis("HorizontalJoystick"));
+        //player moves
+        Vector3 move = Input.GetAxis("HorizontalJoystick") * transform.right + Input.GetAxis("VerticalJoystick")*transform.forward;
+        //Vector3 move = new Vector3(Input.GetAxis("HorizontalJoystick"), 0, Input.GetAxis("VerticalJoystick"));
         cc.Move(move * Time.deltaTime * speed);
 
-        rotation.y += Mathf.Clamp(Input.GetAxis("VerticalJoystick2"), -90, 90);
-        rotation.x += -Input.GetAxis("HorizontalJoystick2");
-        transform.eulerAngles = (Vector2)rotation * rotationSpeed;
+        //player rotates
+        rotation.y += -Input.GetAxis("HorizontalJoystick2");
+        transform.eulerAngles = rotation * rotationSpeed;
+
+        //player head
+        headAngle += Time.deltaTime * headRotationSpeed * -Input.GetAxis("VerticalJoystick2");
+        headAngle = Mathf.Clamp(headAngle, -45, 45);
+        head.localRotation = Quaternion.AngleAxis(headAngle, Vector3.right);
     }
 
 }
