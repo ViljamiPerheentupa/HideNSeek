4f4311bcaa219e5d4ffe7217d11b2a9745bdff3e
Samuel <32210043+Satsaa@users.noreply.github.com>
2022-11-06 12:48:37 +0200

    Power Siphonin start

---
+++ Assets/Scripts/Hider/PowerSiphon.cs
@@ -0,0 +1,19 @@
+using Muc.Components.Extended;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerSiphon : Singleton<PowerSiphon>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
